4bbc80d12484ec51bb40cd4ee3c3734233d5a770
naveedbasharat06 <naveed.basharat06@gmail.com>
2025-02-05 04:49:33 +0500

    QuizPagination Responsiveness Design

--- src/pages/QuizPagination.jsx
+++ src/pages/QuizPagination.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import "./QuizPagination.css";
 import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
@@ -24,32 +24,6 @@ const cardsData = [
     options: ["Definatily", "Maybe", "Not Sure"],
     correct: "Definatily",
   },
-  {
-    id: 3,
-    title: "Achieve Your Dream Figure with Quick and Effective Workouts.",
-    description: "What is the full form of HTML?",
-    subText: "(Knowing basic web terms can help you in development).",
-    options: ["HyperText Markup Language", "HyperType Value", "Not sure"],
-    correct: "HyperText Markup Language",
-  },
-  {
-    id: 4,
-    title: "Achieve Your Dream Figure with Quick and Effective Workouts.",
-    description:
-      "Which of the following activites improves aerobic endurance the most?",
-    subText: "(Knowing basic web terms can help you in development).",
-    options: ["Stretching", "Weightliting", "Swimming", "Archery"],
-    correct: "Swimming",
-  },
-  {
-    id: 4,
-    title: "The ability to maintain equilibrium when stationary or moving:",
-    description:
-      "The ability to maintain equilibrium when stationary or moving:",
-    subText: "(Knowing basic web terms can help you in development).",
-    options: ["Accuracy", "Flexibility", "Balance", "Agility"],
-    correct: "Balance",
-  },
 ];
 
 const PaginatedCards = () => {
@@ -57,6 +31,7 @@ const PaginatedCards = () => {
   const [selectedOption, setSelectedOption] = useState("");
   const [answered, setAnswered] = useState(false);
   const [direction, setDirection] = useState(1);
+
   const handleNext = () => {
     if (currentCardIndex < cardsData.length - 1) {
       setDirection(1);
@@ -69,7 +44,6 @@ const PaginatedCards = () => {
   const handlePrevious = () => {
     if (currentCardIndex > 0) {
       setDirection(-1);
-
       setCurrentCardIndex(currentCardIndex - 1);
       setAnswered(false); // Reset answered state when moving to previous question
       setSelectedOption(""); // Clear selected option
@@ -90,7 +64,7 @@ const PaginatedCards = () => {
   return (
     <div className="main-container">
       <div className="paginationCard-main flex items-center justify-center">
-        <div className="Quiz_Card w-full max-w-md sm:max-w-lg md:max-w-xl lg:max-w- bg-white shadow-lg my-6 p-3">
+        <div className="Quiz_Card bg-white shadow-lg my-0 p-3">
           {/* Card Content */}
           {/* Animated Card */}
           <AnimatePresence custom={direction} mode="wait">
@@ -102,18 +76,24 @@ const PaginatedCards = () => {
               transition={{ type: "spring", stiffness: 150, damping: 30 }}
               className="text-center"
             >
-              <h1 className="Quiz-title  text-gray-800 leading-tight">
+              <h1 className="Quiz-title text-gray-800 leading-tight">
                 {cardsData[currentCardIndex].title}
               </h1>
-              <div className="Quiz-progress-bar bg-gray-300 rounded-full h-5  ">
+              <div className="Quiz-progress-bar bg-gray-300 rounded-full h-5">
                 <div
-                  className={`rounded-l-lg bg-purple-500 h-5 w-full bg-[linear-gradient(90deg,_rgba(233,99,159,1)_0%,_rgba(129,67,226,1)_73%)] ${
-                    progressPercentage === 100 ? "rounded-r-lg" : ""
-                  }`}
-                  style={{ width: `${progressPercentage}%` }}
-                ></div>
+                  className={`rounded-l-lg bg-purple-500 h-5 w-full bg-[linear-gradient(90deg,_rgba(233,99,159,1)_0%,_rgba(129,67,226,1)_73%)]`}
+                  style={{
+                    width: `${
+                      progressPercentage === 50
+                        ? progressPercentage - 35
+                        : progressPercentage - 75
+                    }%`,
+                  }}
+                >
+                  {/* {progressPercentage}% */}
+                </div>
               </div>
-              <p className="Quiz-description text-black-400 font-medium ">
+              <p className="Quiz-description text-black-400 font-medium">
                 {cardsData[currentCardIndex].description}
               </p>
               <p className="Quiz-subText mt-2 text-gray-500 text-x1 mx-4">
@@ -125,12 +105,14 @@ const PaginatedCards = () => {
                   <button
                     key={index}
                     onClick={() => handleOptionSelect(option)}
-                    className={`Quiz-option text-center font-medium rounded-lg border-2 relative  ${
+                    className={`Quiz-option text-center font-medium rounded-lg border-2 relative ${
                       selectedOption === option
                         ? checkAnswer()
-                          ? "bg-green-400 text-white"
-                          : "bg-pink-400 text-white"
-                        : "bg-white text-gray-800 hover:bg-gray-100"
+                          ? "customBtnStyle text-white border-0"
+                          : "customBtnStyle text-white border-0"
+                        : // ? "bg-green-400 text-white"
+                          // : "bg-pink-400 text-white"
+                          "bg-white text-gray-800 hover:bg-gray-100"
                     }`}
                     disabled={answered}
                   >
@@ -152,23 +134,17 @@ const PaginatedCards = () => {
             <button
               onClick={handlePrevious}
               disabled={currentCardIndex === 0}
-              className={`px-4 py-2 text-black font-medium rounded-lg ${
-                currentCardIndex === 0 ? "bg-red-200 cursor-not-allowed" : ""
-              }`}
+              className={`px-4 py-2 text-black font-medium rounded-lg`}
             >
-              <SlArrowLeft style={{ fontSize: "30px" }} />
+              <SlArrowLeft style={{ fontSize: "28px", color: "#575757" }} />
             </button>
-            {`${currentCardIndex + 1} / ${cardsData.length}`}
+            {`${currentCardIndex + 2}/5`}
             <button
               onClick={handleNext}
               disabled={currentCardIndex === cardsData.length - 1}
-              className={`px-4 py-2 text-black font-medium rounded-lg ${
-                currentCardIndex === cardsData.length - 1
-                  ? "bg-red-200 cursor-not-allowed"
-                  : ""
-              }`}
+              className={`px-4 py-2 text-black font-medium rounded-lg`}
             >
-              <SlArrowRight style={{ fontSize: "30px" }} />
+              <SlArrowRight style={{ fontSize: "28px", color: "#575757" }} />
             </button>
           </div>
         </div>
